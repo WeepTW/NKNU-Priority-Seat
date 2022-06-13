d23364b4bbad4bbedebd0634029b3f42871ca2fb
WeepTW <96468355+WeepTW@users.noreply.github.com>
2022-06-14 01:48:20 +0800

    Update app2.py

--- app2.py
+++ app2.py
@@ -9,7 +9,6 @@ from linebot.exceptions import (
 from linebot.models import *
 import re
 
-
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
@@ -38,52 +37,43 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    if re.match('開始',message):
-        line_bot_api.reply_message(  # 回復傳入的訊息文字
-            event.reply_token,TemplateSendMessage(
-                alt_text='Buttons template',
-                template=ButtonsTemplate(
-                title='我的服務',
-                text='請選擇服務項目',
-                actions=[
-                    MessageTemplateAction(
-                    label='我要預約！',
-                    text='我要預約！'
-                    ),
-                    MessageTemplateAction(
-                    label='取消預約',
-                    text='取消預約'
-                    ),
-                    MessageTemplateAction(
-                    label='我自己來',
-                    uri='https://sso.nknu.edu.tw/userLogin/login.aspx?cUrl=/StudentProfile/CreditsNote/ApplyStu.aspx'
-                    )
-                            
-                        ]
-                    )
-                )
-            )
-                    
-    else:
-        if re.match('我要預約！',message):
-            reserve_text = '預約前做個小提醒~依次一個帳號只能借用一個空間\n能借用的空間有:和平-\n1.研究小間 三天前\n2.小型團體(4F、5F)(3人) 七天前\n燕巢-\n1.研究小間 三天前\n2.團體討論室、欣賞室、討論室 七天前\n請用換行方式依序填入下列資料讓我們替您預約空間喔~\n您的學生id:\n您要預約的校區(和平/燕巢):\n您要預約的空間():\n您要預約的時間(今天/明天/後天/大後天 + 時間):'
-            line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(reserve_text))
-            return 0
-        if re.match('取消預約',message):
-            reserve_text = '提醒您 我們會取消所有的借用預約 您確定要取消預約嗎? (若是 請打上: 確定取消 )'
-            line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(reserve_text))
-            return 0
-        if re.match('我自己來',message):
-            return 0
-        if re.match('確定取消',message):
-            #cancel()
-            return 0
-line_bot_api.push_message('Ub7e9f322724c6b15cab6fc57630e5d8c', TextSendMessage(text='功能確認'))              
+    message = text =event.message.text
+    if re.match('開始',message):    
+        buttons_template_message = TemplateSendMessage(
+        alt_text='選擇服務',
+        template=ButtonsTemplate(
+        title='請選擇服務項目',
+        text='選單功能－TemplateSendMessage',
+             actions=[
+                 MessageAction(
+                     label='我要預約！',
+                    text='我要預約！'  
+                 ),
+                 MessageAction(
+                     label='取消預約',
+                     text='取消預約' 
+                 ),
+                 URIAction(
+                     label='我自己來！',
+                     uri='https://lend.nknu.edu.tw/semac/home/index.html'
+                 )
+             ]
+         )
+     )
+        line_bot_api.reply_message(event.reply_token, buttons_template_message)
+    if re.match('我要預約！',message):
+        reserve_text = '預約前做個小提醒~依次一個帳號只能借用一個空間\n能借用的空間有:和平-\n1.研究小間 三天前\n2.小型團體(4F、5F)(3人) 七天前\n燕巢-\n1.研究小間 三天前\n2.團體討論室、欣賞室、討論室 七天前\n請用換行方式依序填入下列資料讓我們替您預約空間喔~\n您的學生id:\n您要預約的校區(和平/燕巢):\n您要預約的空間():\n您要預約的時間(今天/明天/後天/大後天 + 時間):'
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
+        return 0
+    if re.match('取消預約',message):
+        reserve_text = '提醒您 我們會取消所有的借用預約 您確定要取消預約嗎? (若是 請打上: 確定取消)'
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
+        return 0
+    if re.match('我自己來',message):
+        return 0
+    if re.match('確定取消',message):
+        #cancel()
+        return 0
 
 
 #主程式
