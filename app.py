7216691a39c377cd67db343970d84aa8df175030
WeepTW <96468355+WeepTW@users.noreply.github.com>
2022-06-20 17:07:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 import re
-from run.view import record, reservation,cancel,countblock
-
-import os,sys
-sys.path.append(r'.\run')
 
 app = Flask(__name__)
 
@@ -42,8 +38,7 @@ def callback():
 #訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
 
-token = 0
-days = 0
+token = []
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message_list(event):
     message = text =event.message.text
@@ -82,7 +77,7 @@ def handle_message_list(event):
             preview_image_url='https://q410831143.weebly.com/uploads/1/3/3/8/133895349/rpa_orig.jpg')
         line_bot_api.reply_message(event.reply_token, image_message)
     if re.match('我要預約！',message):
-        reserve_text = '預約前做個小提醒~依次一個帳號只能借用一個空間\n能借用的空間有:\n和平校區-\n1.代號 01-研究小間 三天前\n2.代號 02 03-小型團體(4F、5F)(3人) 七天前 \n燕巢校區-\n1.代號 11-研究小間 三天前 \n2.依序代號 12 13 14 15-團體討論室(2A、2B)、欣賞室(2A、2B) 七天前 \n 3.代號 16-討論室(1A、1B、1C、1D、1E)\n\n請輸入您要預約的空間代號\nex.01\n\n如果打錯了 要做新的預約的話 請打「開始」'
+        reserve_text = '預約前做個小提醒~依次一個帳號只能借用一個空間\n能借用的空間有:\n和平校區-\n1.代號 01-研究小間 三天前\n2.代號 02 03-小型團體(4F、5F)(3人) 七天前 \n燕巢校區-\n1.代號 11-研究小間 三天前 \n2.依序代號 12 13 14 15-團體討論室(2A、2B)、欣賞室(2A、2B) 七天前 \n 3.代號 16-討論室(1A、1B、1C、1D、1E)\n請輸入您要預約的空間代號\n如果打錯了 要做新的預約的話 請打「開始」'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
     if re.match('取消預約',message):
         confirm_template_message = TemplateSendMessage(
@@ -105,126 +100,166 @@ def handle_message_list(event):
          )
         line_bot_api.reply_message(event.reply_token, confirm_template_message)
     if re.match('確定取消',message):
-        reserve_text = cancel(line_bot_api.get_profile('<user_id>'))
+        #cancel()
+        reserve_text = '已取消所有預約'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
 
     if re.match('確認預約情形',message):
-        reserve_text = record(line_bot_api.get_profile('<user_id>'))
+        #record()
+        reserve_text = '你目前所有的預約如下:'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-    
-    for token in token:
-        for day in days: #現在 明天中午 明天下午
-            if re.match(token + ' ' + '現在'):
-                reservation(line_bot_api.get_profile('<user_id>'),token)
-            ds = {0:'今天',1:'明天',2:'後天'}
-            for d in ds:
-                for t in ['早上','中午','晚上']:
-                    if re.match(token + ' ' + d+t):
-                        reservation(id,token,days= ds.index(d),min= '0')
-            if re.match(token + ' ' +day):
-                if day == 0:
-                    reservation(id,token)
-                else:
-                    reservation(id,token,days = day, hour = '08',min = '00')
-    
     if re.match('01',message) or re.match('０１',message):
-        token = 23
+        token.append(23)
         reserve_text = '請選擇您要預約的時間(預約時間辦法請參考圖片-打上「圖片」)'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+        
+        return token
     if re.match('02',message) or re.match('０２',message):
-        token = 26
+        token.append(26)
         reserve_text = '請選擇您要預約的時間(預約時間辦法請參考圖片-打上「圖片」)'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+        
+        return token
     if re.match('03',message) or re.match('０３',message):
-        token = 27
+        token.append(27)
         reserve_text = '請選擇您要預約的時間(預約時間辦法請參考圖片-打上「圖片」)'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
         
+        return token
     if re.match('11',message) or re.match('１１',message):
-        token = 19
+        token.append(19)
         reserve_text = '請選擇您要預約的時間(預約時間辦法請參考圖片-打上「圖片」)'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
         
+        return token
     if re.match('12',message) or re.match('１２',message):
-        token = 5
+        token.append(5)
         reserve_text = '請選擇您要預約的時間(預約時間辦法請參考圖片-打上「圖片」)'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
       
+        return token
     if re.match('13',message) or re.match('１３',message):
-        token = 6
+        token.append(6)
         reserve_text = '請選擇您要預約的時間(預約時間辦法請參考圖片-打上「圖片」)'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
    
+        return token
     if re.match('14',message) or re.match('１４',message):
-        token = 7
+        token.append(7)
         reserve_text = '請選擇您要預約的時間(預約時間辦法請參考圖片-打上「圖片」)'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
    
+        return token
     if re.match('15',message) or re.match('１５',message):
-        token = 8
+        token.append(8)
         reserve_text = '請選擇您要預約的時間(預約時間辦法請參考圖片-打上「圖片」)'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+  
+        return token
     if re.match('16',message) or re.match('１６',message):
-        token = 32
+        token.append(32)
         reserve_text = '請選擇您要預約的時間(預約時間辦法請參考圖片-打上「圖片」)'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
  
+        return token
     if re.match('現在',message):
-        reserve_text = reservation(line_bot_api.get_profile('<user_id>'),token)
+        token.append(1000)
+        reserve_text = '幫你處理囉~'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+        return token
     if re.match('今天上午',message):
-        reserve_text = reservation(line_bot_api.get_profile('<user_id>'),token,days = 0,hour = '7',min = '55')
+        token.append(1008)
+        reserve_text = '幫你處理囉~'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+        return token
     if re.match('今天中午',message):
-        reserve_text = reservation(line_bot_api.get_profile('<user_id>'),token,days = 0,hour = '11',min = '30')
+        token.append(1012)
+        reserve_text = '幫你處理囉~'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+        return token
     if re.match('今天晚上',message):
-        reserve_text = reservation(line_bot_api.get_profile('<user_id>'),token,days = 0,hour = '16',min = '30')
+        token.append(1016)
+        reserve_text = '幫你處理囉~'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+        return token
     if re.match('明天上午',message):
-        reserve_text = reservation(line_bot_api.get_profile('<user_id>'),token,days = 1,hour = '7',min = '55')
+        token.append(1108)
+        reserve_text = '幫你處理囉~'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+        return token
     if re.match('明天中午',message):
-        reserve_text = reservation(line_bot_api.get_profile('<user_id>'),token,days = 1,hour = '11',min = '30')
+        token.append(1112)
+        reserve_text = '幫你處理囉~'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+        return token
     if re.match('明天晚上',message):
-        reserve_text = reservation(line_bot_api.get_profile('<user_id>'),token,days = 1,hour = '16',min = '30')
+        token.append(1116)
+        reserve_text = '幫你處理囉~'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+        return token
     if re.match('後天上午',message):
-        reserve_text = reservation(line_bot_api.get_profile('<user_id>'),token,days = 2,hour = '7',min = '55')
+        token.append(1208)
+        reserve_text = '幫你處理囉~'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+        return token
     if re.match('後天中午',message):
-        reserve_text = reservation(line_bot_api.get_profile('<user_id>'),token,days = 2,hour = '11',min = '30')
+        token.append(1212)
+        reserve_text = '幫你處理囉~'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+        return token
     if re.match('後天晚上',message):
-        reserve_text = reservation(line_bot_api.get_profile('<user_id>'),token,days = 2,hour = '16',min = '30')
+        token.append(1216)
+        reserve_text = '幫你處理囉~'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+        return token
     if re.match('大後天上午',message):
-        reserve_text = reservation(line_bot_api.get_profile('<user_id>'),token,days = 3,hour = '7',min = '55')
+        token.append(1308)
+        reserve_text = '幫你處理囉~'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+        return token
     if re.match('大後天中午',message):
-        reserve_text = reservation(line_bot_api.get_profile('<user_id>'),token,days = 3,hour = '11',min = '30')
+        token.append(1312)
+        reserve_text = '幫你處理囉~'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
-
+        return token
     if re.match('大後天晚上',message):
-        reserve_text = reservation(line_bot_api.get_profile('<user_id>'),token,days = 3,hour = '16',min = '30')
+        token.append(1316)
+        reserve_text = '幫你處理囉~'
         line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
+        return token
+    
+    #學生id匯入
+    if line_bot_api.get_profile('<user_id>') == 'Ub7e9f322724c6b15cab6fc57630e5d8c':
+        stu_id = '410831143'
+        token.append(stu_id)
+        return token
 
+    #token給凡葦        包含:學號 預約空間 預約時間
 
+    #if success:
+    #    reserve_text = '已順利預約囉'
+    #    line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
+    #if TokenError:
+    #    reserve_text = '好像出了點狀況 請再試一次'
+    #    line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
+    #if DaysError:
+    #    reserve_text = '好像出了點狀況 請再試一次'
+    #    line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
+    #if RentTimeerror_Chaos:
+    #    reserve_text = '好像出了點狀況 請再試一次'
+    #    line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
+    #if RentTimeerror_Overtime:
+    #    reserve_text = '好像出了點狀況 請再試一次'
+    #    line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
+    #if TimeException:
+    #    reserve_text = '好像出了點狀況 請再試一次'
+    #    line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
+    #if FullHouseError:
+    #    reserve_text = '好像出了點狀況 請再試一次'
+    #    line_bot_api.reply_message(event.reply_token,TextSendMessage(reserve_text))
+    
+
+import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
